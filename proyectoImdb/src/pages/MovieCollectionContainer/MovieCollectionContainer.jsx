af389a03efc420d865b8512a72c30c2ff9d7f9e2
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-08-05 17:02:27 -0300

    feat: added api

--- proyectoImdb/src/pages/MovieCollectionContainer/MovieCollectionContainer.jsx
+++ proyectoImdb/src/pages/MovieCollectionContainer/MovieCollectionContainer.jsx
@@ -1,30 +1,31 @@
 import { useEffect, useState } from "react";
-import MovieCollection from "../../components/Collection/MovieCollection";
+// import MovieCollection from "../../components/Collection/MovieCollection";
+import Movie from "../../components/Movie/Movie";
 
 const MovieCollectionContainer = () => {
   const [movies, setMovies] = useState([]);
 
   const api =
-    "https://api.themoviedb.org/3/list/1?api_key=281ba2168b1ae32fe373f4735451daa8";
+    "https://api.themoviedb.org/4/list/1?api_key=281ba2168b1ae32fe373f4735451daa8";
 
   useEffect(() => {
     const getMovie = async () => {
       const response = await fetch(api);
       const data = await response.json();
 
-      console.log(data.items[0]);
-      setMovies(data.items);
-      movies.forEach((el) => {
-        const { poster_path } = el;
-        console.log(poster_path);
-      });
+      console.log(data.results);
+      setMovies(data.results);
     };
     getMovie();
   }, []);
 
   return (
     <div>
-      <MovieCollection movies={movies} />
+      {/* <MovieCollection movies={movies} /> */}
+      {/* //         <Movie key={movie.id} movie={movie} /> */}
+      {movies.map((movie) => (
+        <Movie key={movie.id} movie={movie} />
+      ))}
     </div>
   );
 };
